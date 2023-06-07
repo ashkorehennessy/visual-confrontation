1f335deda0d170ff8c65b8780cb5488c5d0a27b5
ashkorehennessy <ashkorehennessy@gmail.com>
2023-06-08 05:21:19 +0800

    更新pid

--- pid.py
+++ pid.py
@@ -1,10 +1,10 @@
 class PID:
     def __init__(self,Kp=1,Kd=0,outmax=100,outmin=-100):
-        self.Kp = 0.2
-        self.Kd = 0
+        self.Kp = Kp
+        self.Kd = Kd
         self.last_error = 0
-        self.outmax = 250
-        self.outmin = -250
+        self.outmax = outmax
+        self.outmin = outmin
 
     def update(self, input_value, setpoint):
         error = setpoint - input_value
